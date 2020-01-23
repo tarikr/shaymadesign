50759806ab44d43e3c058d57584aed58aefdd5cc
trazak <trazak@outlook.com>
2020-01-22 16:35:05 -0800

    broken our homepage into smaller components, finished styling our menu-item components

---
+++ shaymadesign/src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../components/directory/directory.component';
+
+//functional component because no lifecycle methods needed tat this time
+//nor we dont need any state 
+const HomePage = () => (
+ <Directory />
+);
+
+export default HomePage;
