121436e29c668c42995e3d1458ba8267d58db76a
trazak <trazak@outlook.com>
2020-01-24 03:01:41 -0800

    added header component and links to home shop page and contact

--- shaymadesign/src/App.js
+++ shaymadesign/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component.jsx';
 const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
@@ -15,10 +15,12 @@ const HatsPage = () => (
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
     <Route exact path='/' component={HomePage} />
     <Route path='/shop' component={ShopPage} />
     </Switch>
+
     </div>
   );
 }
