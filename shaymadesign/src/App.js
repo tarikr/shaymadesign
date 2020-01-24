9df3ee330eb28f94bcb288be80d7f99504103a1f
trazak <trazak@outlook.com>
2020-01-24 02:35:28 -0800

    updating app with shop page, collection preview collection item

--- shaymadesign/src/App.js
+++ shaymadesign/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
 import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,7 @@ function App() {
     <div >
       <Switch>
     <Route exact path='/' component={HomePage} />
-    <Route path='/hats' component={HatsPage} />
+    <Route path='/shop' component={ShopPage} />
     </Switch>
     </div>
   );
