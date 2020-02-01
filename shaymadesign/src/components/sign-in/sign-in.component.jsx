9a3d0f31be0a49a44c4a2b267f7cdfc79ba93f28
trazak <trazak@outlook.com>
2020-02-01 03:12:43 -0800

    finishing sign in handle method

--- shaymadesign/src/components/sign-in/sign-in.component.jsx
+++ shaymadesign/src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -11,11 +11,16 @@ class SignIn extends React.Component{
             password: ''
         }
     }
- handleSubmit = event=>{
+ handleSubmit = async event=>{
      event.preventDefault();
-     this.setState({email: '', password: ''})
-
- }
+    const {email, password}= this.state;
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
+    this.setSate({email: '', password: ''})
+    } catch(error){
+        console.log(error);
+    }
+ };
 
  handleChange = event =>{
      const {value, name} = event.target;
@@ -32,7 +37,7 @@ class SignIn extends React.Component{
             <form onSubmit={this.handleSubmit}>
                 <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} required label="email" />
          
-                 <FormInput name="password" type="password" value={this.state.email} handleChange={this.handleChange} label="password" required/>
+                 <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
                   <div className='buttons'>
                  <CustomButton type='submit'>Sign In</CustomButton>
                  <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
