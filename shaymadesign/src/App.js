4eb347bdc4bc6810be60e1ba2b148b88721db0a0
trazak <trazak@outlook.com>
2020-02-01 02:59:48 -0800

    implemented firebase utils and adding authenticated user

--- shaymadesign/src/App.js
+++ shaymadesign/src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from  './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -23,11 +23,26 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
+  //user login and log out
   componentDidMount() {
 
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if(userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
+          userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:  {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+           });
+         console.log(this.state);
+        });
+      }
+        this.setState({ currentUser: userAuth});
+  // this.setState({currentUser: user});
+ // createUserProfileDocument(user);
+ //     console.log(user);
     }); 
   }
 
